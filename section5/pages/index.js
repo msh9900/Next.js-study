a10d38324e41844a5c22ce5931c65596cac92522
MOS <msh9900@naver.com>
2023-01-16 17:38:03 +0900

    section5,6

---
+++ section5/pages/index.js
@@ -0,0 +1,44 @@
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  console.log("재생성중...");
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destinaition: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 60, //10초가 지나면 재생성 되어야한다고 next.js에 말하는 기능
+  };
+}
+
+export default HomePage;
---
+++ section5/pages/last-sales.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-study-fc375-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://nextjs-study-fc375-default-rtdb.firebaseio.com/sales.json")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+  if (!data && !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username}-{sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-study-fc375-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return { props: { sales: transformedSales }, revalidate: 10 };
+}
